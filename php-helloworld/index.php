16de61f8efabe7f48cf518c62b97b659093b5f32
PEscorza <pescorza@redhat.com>
2022-11-27 10:53:04 +0100

    Zac message

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,6 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Zac dice: Hola! TE QUIERO !  " . PHP_VERSION . "\n";
+print "Mama dice: Yo también mi amor  TE QUIERO !  " . PHP_VERSION . "\n";
+
 ?>
